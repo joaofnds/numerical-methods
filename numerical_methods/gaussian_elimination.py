f79388a90bf8f350957072a0eb881fa1699c76fc
João Fernandes <joaofnds@joaofnds.com>
2024-06-23 17:58:24 -0300

    rename loop variable

--- numerical_methods/gaussian_elimination.py
+++ numerical_methods/gaussian_elimination.py
@@ -2,21 +2,21 @@ import numpy as np
 
 
 def gaussian_elimination(A: np.ndarray, b: np.ndarray):
-    Ab = np.hstack((A, b.reshape(-1, 1)))
+    Ab = np.column_stack((A, b))
     nrows = len(b)
 
     # forward elimination
-    for i in range(nrows):
-        max_row = np.argmax(np.abs(Ab[i:, i])) + i  # find row with max value in the i-th column
-        Ab[[i, max_row]] = Ab[[max_row, i]]
+    for row in range(nrows):
+        max_row = abs(Ab[row:, row]).argmax() + row
+        Ab[[row, max_row]] = Ab[[max_row, row]]
 
-        for k in range(i + 1, nrows):
-            factor = Ab[k, i] / Ab[i, i]
-            Ab[k, :] -= factor * Ab[i, :]
+        for k in range(row + 1, nrows):
+            factor = Ab[k, row] / Ab[row, row]
+            Ab[k] -= factor * Ab[row]
 
     # back substitution
     x = np.zeros(nrows)
-    for i in range(nrows - 1, -1, -1):
-        x[i] = (Ab[i, nrows] - np.dot(Ab[i, i + 1 : nrows], x[i + 1 : nrows])) / Ab[i, i]
+    for row in range(nrows - 1, -1, -1):
+        x[row] = (Ab[row, nrows] - np.dot(Ab[row, row + 1 : nrows], x[row + 1 : nrows])) / Ab[row, row]
 
     return x
