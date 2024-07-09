5c84b462fc26d72124c53b983a67c95e6df9a74f
João Fernandes <joaofnds@joaofnds.com>
2024-07-09 02:13:49 -0300

    remove duplicate code

--- numerical_methods/newton_raphson_trail.py
+++ numerical_methods/newton_raphson_trail.py
@@ -1,4 +1,3 @@
-from numerical_methods.find_root import trunc
 from numerical_methods.trail import Trail
 
 
@@ -19,24 +18,3 @@ class NewtonRaphsonTrail(Trail):
     @property
     def bounds(self):
         return (self[-1][0], self[0][0])
-
-
-def newton_raphson(f, fd, x, tolerance=None, max_iterations=None, truncate=True, precision=4):
-    cap = trunc if truncate else round
-    trail = NewtonRaphsonTrail(f, fd, cap, precision)
-
-    i = 1
-    while True:
-        trail.record(x)
-
-        if tolerance is not None and abs(f(x)) < tolerance:
-            break
-
-        if max_iterations is not None and i >= max_iterations:
-            break
-
-        x = cap(x - (f(x) / fd(x)), precision)
-
-        i += 1
-
-    return trail
